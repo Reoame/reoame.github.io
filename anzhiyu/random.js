6993c5b3ad512495782697423816450dddb200af
Reoame <littlechengoodluck@hotmail.com>
2025-03-09 14:31:48 +0800

    Site updated: 2025-03-09 14:31:48

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/03/08/hello-world/","2025/03/09/2/","2025/03/09/关于GitHub无法访问的解决办法/","2025/03/09/post/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
